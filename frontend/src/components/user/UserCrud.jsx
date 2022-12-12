f81804b6c5da6e5230bcfebe5918b4ee4635b0c9
MateusGomes04 <mateusgomesc04@gmail.com>
2022-12-12 18:44:51 -0300

    add React Router

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Main from '../template/Main'
+import Main from "../templetes/Main";
 
 const headerProps = {
     icon: 'users',
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,17 +2,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
+import Routes from './Router'
 import Logo from '../components/templetes/Logo'
 import Nav from '../components/templetes/Nav'
-import Home from '../components/home/Home'
 import Footer from '../components/templetes/Footer'
 
 export default props =>
-    <div className='app'>
-        <Logo />
-        <Nav />
-        <Home />
-        <Footer />
-    </div>
+<BrowserRouter>
+<div className='app'>
+    <Logo />
+    <Nav />
+    <Routes />
+    <Footer />
+</div>
+</BrowserRouter>
+
+
 
